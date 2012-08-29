57aa914fb9e7f23e7896ca0a3f53d8e9a3e501df
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2012-08-29 20:17:30 +0300

    Make use of the new fix16_to_str() from libfixmath

--- fixstring.c
+++ fixstring.c
@@ -1,33 +1,14 @@
 #include "fixstring.h"
-
-static const unsigned scales[6] = {
-    1, 10, 100, 1000, 10000, 100000
-};
+#include <string.h>
 
 void print_fix16_t(FILE *stream, fix16_t value, uint_fast8_t width, uint_fast8_t decimals)
 {
-    unsigned scale = scales[decimals];
-    value = fix16_mul(value, scale);
-    
-    char fmt[5] = "%01d";
-    fmt[2] = decimals + (value < 0 ? '2' : '1');
-    char buf[20];
-    int len = snprintf(buf, sizeof(buf), fmt, value);
-    
-    if (decimals) width--;
-    width -= len;
+    char buf[13];
+    fix16_to_str(value, buf, decimals);
+    width -= strlen(buf);
     while (width-- > 0) fprintf(stream, " ");
-    
-    char tmp = buf[len - decimals];
-    buf[len - decimals] = 0;
-    
-    fprintf(stream, "%s", buf);
-    
-    if (decimals != 0)
-    {
-        buf[len - decimals] = tmp;
-        fprintf(stream, ".%s", &buf[len - decimals]);
-    }
+
+    fputs(buf, stream);
 }
 
 void print_mf16(FILE *stream, const mf16 *matrix)
